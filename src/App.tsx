72031e8810d634948d45baabdf4d5c2c8825f0a1
Weslley Rodrigues <weslley.rdgs@gmail.com>
2023-01-25 17:06:50 -0300

    implementa componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import Habit from './components/Habit'
+
+
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
-      <Habit completed={10}/>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header/>
+      </div>
     </div>
   )
 }
--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -3,14 +3,13 @@ import styles from './Habit.module.css'
 
 import '../styles/global.css'
 
-
 interface HabitProps {
   completed: number
 }
 
 function Habit({completed}: HabitProps) {
   return (
-    <div className="bg-zinc-900">{completed}</div>
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2">{completed}</div>
   )
 }
 
