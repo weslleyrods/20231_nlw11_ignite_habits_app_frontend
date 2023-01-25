dfb3ffa39267ea33dc101a6cdf0f696eb506fdad
Weslley Rodrigues <weslley.rdgs@gmail.com>
2023-01-25 20:41:05 -0300

    implementa range de dias

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 
 
 import Header from './components/Header'
+import Summary from './components/Summary'
 
 function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
         <Header/>
+        <Summary/>
       </div>
     </div>
   )
 }
 
-export default App
+export default App 
