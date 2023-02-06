6a207432d52fef88577dc92ed60e0a4f5f19821d
Weslley Rodrigues <weslley.rdgs@gmail.com>
2023-02-06 18:37:22 -0300

    implementa checkbox em HabitDay e NewHabitForm

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,7 +1,20 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 import React from 'react'
 
+const availableWeeDays =[
+  'Dmoningo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+]
+
 function NewHabitForm() {
+
+  
   return (
     <form className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
@@ -17,6 +30,28 @@ function NewHabitForm() {
       <label htmlFor=""  className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label> 
+        <div className='mt-3 flex flex-col gap-2'>
+          {availableWeeDays.map((weekDay)=>{
+            return (
+              <Checkbox.Root
+                key={weekDay}
+                className='flex items-center gap-3 group'
+              >
+                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                  <Checkbox.Indicator>
+                    <Check size={20} className='text-white'/>
+                  </Checkbox.Indicator>
+                </div>
+
+                <span className='text-white leading-tight'>
+                  {weekDay}
+                </span>
+              </Checkbox.Root>
+            )
+          })}
+
+  
+        </div>
       <button type="submit" className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500">
         <Check size={20} weight="bold"/>
         Confirmar
